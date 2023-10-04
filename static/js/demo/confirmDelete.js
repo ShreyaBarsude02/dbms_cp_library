b9c718afb3b692a034e7cf99fc6204473f807a7a
SamarthBagde <bagdesamarth70@gmail.com>
2023-10-04 22:43:57 +0530

    Update confirmDelete.js

--- static/js/demo/confirmDelete.js
+++ static/js/demo/confirmDelete.js
@@ -1,15 +1,24 @@
-
 function deleteConfirm() {
-    let confirmDelete = confirm("Delete book");
-    if (confirmDelete) {
-        document.getElementById('confirm').value = "yes";
-        document.getElementById('form').submit;
-    }
+  let confirmDelete = confirm("Delete book");
+  if (confirmDelete) {
+    document.getElementById("confirm").value = "yes";
+    document.getElementById("form").submit;
+  }
 }
 function issueConfirm() {
-    let confirmDelete = confirm("Issue book");
-    if (confirmDelete) {
-        document.getElementById('confirm').value = "yes";
-        document.getElementById('issueForm').submit;
-    }
+  let confirmDelete = confirm("Issue book");
+  if (confirmDelete) {
+    document.getElementById("confirm").value = "yes";
+    document.getElementById("issueForm").submit;
+  }
+}
+
+function goBack() {
+  // Using the history object to go back to the previous page
+  window.history.back();
+}
+
+function closeAlert() {
+  var alert = document.querySelector('.alert');
+  alert.style.display = 'none';
 }
